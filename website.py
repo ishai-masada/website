fdf6b71e8ae8867dc0f94c4acd771143fdcff98b
root <root@AEIM.localdomain>
2023-12-15 21:49:18 -0500

    added website file
    Transitioning from using django framework to using webbrowser python module and .html file format

---
+++ website.py
@@ -0,0 +1,9 @@
+import webbrowser
+import os
+
+#filename = os.getcwd() + '/' + 'backend.html' 
+filename = 'backend.html' 
+url = 'https://www.beta.team/careers/'
+chrome_path = "C:/Users/ipmas/AppData/Local/Google/Chrome/Application/chrome.exe" 
+webbrowser.get(chrome_path)
+#webbrowser.open_new_tab(filename)
